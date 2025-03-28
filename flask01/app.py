9727b04afd48121c44bb89322468b79f5dc6a0f4
ksy0828 <kim42348@gmail.com>
2025-03-28 10:03:13 +0900

    0328rss

--- flask01/app.py
+++ flask01/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request
+import feedparser
+
+#https://www.dailysecu.com/rss/allArticle.xml
 
 app = Flask(__name__)
 
 @app.route("/") #라우터..패킷들을 최적화시켜서 도착하게 하는 것..?
-def hello_world():
+def index():
     return render_template("index.html")
 
 @app.route("/rss", methods=['GET', 'POST']) 
 def rss():
     rss_url = request.form['rss_url']
-    print(rss_url)
-    return render_template("rss.html")
+    # print(rss_url)
+    feed = feedparser.parse(rss_url)
+    # print(feed)
+    return render_template("rss.html", feed=feed) #'보낼때 변수명'='원래사용한변수' 
     
 if __name__ == '__main__':
     app.run(debug=True)
